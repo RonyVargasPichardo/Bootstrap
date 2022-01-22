dcba892800b8b41ce1b05f62b7ccc969a1fd5a13
Rony Vargas <vargaspihardo@gmail.com>
2022-01-21 22:45:48 -0400

    agregando tooltips a la palabra ignite que es una charla corta

---
+++ index.js
@@ -0,0 +1,8 @@
+        
+        
+//<----! habilitando los tooltip en mi html  !--------->//
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
